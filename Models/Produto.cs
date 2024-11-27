b8ea9c97ab40585513a3c47fd7165f776c23c154
Wester Morandi <westerjesuino@gmail.com>
2024-11-27 01:00:37 -0300

    Refactor Produto model to use simplified property names and add Descricao field

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -2,8 +2,9 @@
 
 public class Produto
 {
-    public int IdProduto { get; set; }
+    public int Id { get; set; }
     public string Nome { get; set; }
+    public string Descricao { get; set; }
     public decimal Preco { get; set; }
     public int QuantidadeEstoque { get; set; }
     public DateTime? DataValidade { get; set; }
